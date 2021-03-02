a73bda7dc5d11fe64564166df4ad9ee5f92cf5fb
Igor <80776287+i9or@users.noreply.github.com>
2021-03-02 23:09:35 +0100

    Draw sprite

--- src/chapter1/chapter1.cpp
+++ src/chapter1/chapter1.cpp
@@ -1,12 +1,18 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "First window!");
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Bouncing mushroom!");
 
-    sf::RectangleShape rectangle(sf::Vector2f(128.0f, 128.0f));
-    rectangle.setFillColor(sf::Color::Red);
-    rectangle.setPosition(320, 240);
-    rectangle.setOrigin(rectangle.getSize().x / 2, rectangle.getSize().y / 2);
+    sf::Texture mushroomTexture;
+    mushroomTexture.loadFromFile("../assets/mushroom.png");
+    sf::Sprite mushroom(mushroomTexture);
+    sf::Vector2u size = mushroomTexture.getSize();
+    mushroom.setOrigin(size.x / 2.0f, size.y / 2.0f);
+    sf::Vector2f increment(0.4f, 0.4f);
+
+    unsigned short alpha{};
+    unsigned short alphaIncrement = 1;
 
     while (window.isOpen()) {
         sf::Event event{};
@@ -16,9 +22,32 @@ int main() {
             }
         }
 
+        if ((mushroom.getPosition().x + (size.x / 2.0f) > window.getSize().x &&
+             increment.x > 0) ||
+            (mushroom.getPosition().x - (size.x / 2.0f) < 0 &&
+             increment.x < 0)) {
+            increment.x = -increment.x;
+        }
+
+        if ((mushroom.getPosition().y + (size.y / 2.0f) > window.getSize().y &&
+             increment.y > 0) ||
+            (mushroom.getPosition().y - (size.y / 2.0f) < 0 &&
+             increment.y < 0)) {
+            increment.y = -increment.y;
+        }
+
+        alpha += alphaIncrement;
+
+        if (alpha < 0 || alpha > 255) {
+            alphaIncrement = -alphaIncrement;
+        }
+
+        mushroom.setPosition(mushroom.getPosition() + increment);
+        mushroom.setColor(sf::Color(255, 255, 255, alpha));
+
         window.clear(sf::Color::Black);
 
-        window.draw(rectangle);
+        window.draw(mushroom);
 
         window.display();
     }
