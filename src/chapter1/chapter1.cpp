25ff956cee5e2d772970e5f295206535cc4ac730
Igor <80776287+i9or@users.noreply.github.com>
2021-03-02 22:34:38 +0100

    Add shapes drawing

--- src/chapter1/chapter1.cpp
+++ src/chapter1/chapter1.cpp
@@ -3,6 +3,11 @@
 int main() {
     sf::RenderWindow window(sf::VideoMode(640, 480), "First window!");
 
+    sf::RectangleShape rectangle(sf::Vector2f(128.0f, 128.0f));
+    rectangle.setFillColor(sf::Color::Red);
+    rectangle.setPosition(320, 240);
+    rectangle.setOrigin(rectangle.getSize().x / 2, rectangle.getSize().y / 2);
+
     while (window.isOpen()) {
         sf::Event event{};
         while (window.pollEvent(event)) {
@@ -13,7 +18,7 @@ int main() {
 
         window.clear(sf::Color::Black);
 
-        // Draw here
+        window.draw(rectangle);
 
         window.display();
     }
