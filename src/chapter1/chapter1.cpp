0ffad902be35565e0898e15eb40a4841ede5c282
Igor <80776287+i9or@users.noreply.github.com>
2021-03-02 20:06:08 +0100

    Add basic SFML application

---
+++ src/chapter1/chapter1.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "First window!");
+
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+        // Draw here
+
+        window.display();
+    }
+
+    return 0;
+}
